bf9987853ab3bba882772336b57b9e89877effe3
thiagomeneguzzi <thiagonmeneguzzi@gmail.com>
2022-06-13 23:44:46 -0300

    created like-widget

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { UniqueIdService } from './shared/services/unique-id/unique-id.service';
+import { LikeWidgetComponent } from './shared/components/like-widget/like-widget.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LikeWidgetComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule
   ],
-  providers: [],
+  providers: [UniqueIdService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
