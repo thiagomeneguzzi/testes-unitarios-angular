ff1c0eb443931456368b302bd8fa86c24d64548a
thiagomeneguzzi <thiago.meneguzzi@alunos.sc.senac.br>
2022-06-17 14:09:03 -0300

    improved tests description

--- src/app/shared/components/like-widget/like-widget.component.spec.ts
+++ src/app/shared/components/like-widget/like-widget.component.spec.ts
@@ -28,19 +28,19 @@ describe(`${LikeWidgetComponent.name}`, () => {
         expect(component).toBeTruthy()       
     })
 
-    it(`Should auto generate ID when id input property is missing`, () => {
+    it(`Should auto-generate ID during ngOnInit when (@Input id) is not assigned`, () => {
         fixture.detectChanges();
         expect(component.id).toBeTruthy();
     })
 
-    it(`Should NOT generate ID when id input property is present`, () => {
+    it(`Should NOT auto-generate ID during ngOnInit when (@Input id) is assigned`, () => {
         const someId = new UniqueIdService().generateUniqueIdWithPrefix('someid')
         component.id = someId;
         fixture.detectChanges()
         expect(component.id).toBe(someId);
     })
 
-    it(`#${LikeWidgetComponent.prototype.like} should trigger emission when called`, () => {
+    it(`#${LikeWidgetComponent.prototype.like} should trigger (@Output liked) when called`, () => {
         spyOn(component.liked, 'emit');
         fixture.detectChanges();
         component.like();
